5d017b0f31f793975d4e4f4901a887a767bc8744
TeddyGavi <matc.davis@gmail.com>
2022-12-04 10:02:03 -0800

    finish day4, thinking about moving tests into separate folder and add for utils for counting arrays, and removing duplicate etc

--- day4/__test__/day4.test.ts
+++ day4/__test__/day4.test.ts
@@ -1,7 +1,15 @@
 import { assert } from "chai";
+import { inputContains, inputOverlaps, sampleContains } from "../day4";
 
 describe("Day 4 this is getting harder...", () => {
   it("Should correctly id the amount of fully contained pairs", () => {
-    assert.equal(0, 2);
+    assert.equal(sampleContains, 2);
+  });
+
+  it("Should be correct for part one", () => {
+    assert.equal(inputContains, 582);
+  });
+  it("Should be correct for part two", () => {
+    assert.equal(inputOverlaps, 893);
   });
 });
--- day4/day4.ts
+++ day4/day4.ts
@@ -35,17 +35,65 @@ no that won't work, we should just compare each new array with arr.every(x => ar
 return true or false, then convert that to 1 or 0 then sum the array
 */
 
-import { sample4 } from "./input4";
+import { sample4, input4 } from "./input4";
+import { sum } from "../utils/utils";
 
 // number[]
-export function fullyContains(groups: string) {
+function parseToArrPairs(groups: string): string[][][] {
   const group = groups.trim().split("\n");
-  console.log(
-    group.map((x) => {
-      const num = x.replace(/[,]/g, ",").split(",");
-      console.log(num);
+  const pairs = group.map((x: string): string[] => {
+    return x.replace(/[,]/g, ",").split(",");
+  });
+  return pairs.map((x: string[]) => {
+    return x.map((x: string) => x.split("-"));
+    // console.log(first, second);
+  });
+}
+function doesContain(pairArr: string[][][]) {
+  let sum = 0;
+  return pairArr
+    .map((x) => {
+      const [x1, x2] = x[0];
+      const [y1, y2] = x[1];
+      let doesIt =
+        (Number(x1) <= Number(y1) && Number(x2) >= Number(y2)) ||
+        (Number(y1) <= Number(x1) && Number(y2) >= Number(x2))
+          ? true
+          : false;
+      return doesIt;
     })
-  );
+    .map((x) => (x ? 1 : sum))
+    .filter((x) => x)!.length;
 }
 
-console.log(fullyContains(sample4));
+const parsedSample = parseToArrPairs(sample4);
+export const sampleContains: number = doesContain(parsedSample);
+const parsedInput = parseToArrPairs(input4);
+export const inputContains: number = doesContain(parsedInput);
+// console.log(parseToArrPairs(sample4)); // 2
+// console.log(doesContain(parseToArrPairs(input4))); // 582
+// console.log(parsedSample);
+function doesOverlap(pairs: string[][][]) {
+  return pairs
+    .map((x) => {
+      const buildX: number[] = [];
+      const buildY: number[] = [];
+      const [x1, x2] = x[0];
+      const [y1, y2] = x[1];
+      for (let i = +x1; i <= +x2; i++) {
+        buildX.push(i);
+      }
+      for (let i = +y1; i <= +y2; i++) {
+        buildY.push(i);
+      }
+      return [buildX, buildY];
+    })
+    .map((x) => {
+      return x[0].filter((y) => x[1].includes(y));
+    })
+    .map((x) => x.length)
+    .filter((x) => x).length;
+}
+
+// console.log(doesOverlap(parsedInput));
+export const inputOverlaps: number = doesOverlap(parsedInput);
